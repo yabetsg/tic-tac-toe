5ff2e0c11efef6a4457548658b126cccfe09982f
yabets <yabets.g2@gmail.com>
2023-03-09 16:33:06 -0500

    Fix issue where a tie would show on wins

--- script.js
+++ script.js
@@ -1,111 +1,107 @@
-const grids = document.querySelectorAll('.grids');
-const playagain = document.querySelector('.playagain');
-const turnDisplay = document.querySelector('.turn');
-const winningDisplay = document.querySelector('.winning-display');
-const winningMessage = document.querySelector('.winning-message .turn');
-const afterWinPlayAgain = document.querySelector('.restart');
-const winDiv = document.querySelector('.wins-div');
-const players = (symbol) =>{
-    let turn = true;
-   
-    const getSymbol = ()  =>{
-       return symbol;
-    }
-    return {getSymbol,turn};
+const getSelectors = (() => ({
+  grids: document.querySelectorAll('.grids'),
+  playagain: document.querySelector('.playagain'),
+  turnDisplay: document.querySelector('.turn'),
+  winningDisplay: document.querySelector('.winning-display'),
+  winningMessage: document.querySelector('.winning-message .turn'),
+  afterWinPlayAgain: document.querySelector('.restart'),
+  winDiv: document.querySelector('.wins-div'),
+}))();
+
+const players = (symbol) => {
+  const turn = true;
+
+  const getSymbol = () => symbol;
+  return { getSymbol, turn };
 };
 
+const updateScreen = (() => {
+  const refresh = () => {
+    getSelectors.grids.forEach((element) => {
+      const e = element;
+      e.textContent = '';
+    });
+  };
+  return { refresh };
+})();
 
+const checkForTie = (gridcount) => {
+  if (gridcount >= 9) {
+    getSelectors.winningDisplay.classList.add('show');
+    getSelectors.winningMessage.textContent = 'ITS A TIE!';
+    getSelectors.winDiv.textContent = '';
+  }
+};
 
- 
+const checkWin = (() => {
+  const winCombinations = [[0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  // eslint-disable-next-line consistent-return
+  const checkWinningPlayer = (symbol) => {
+    for (let i = 0; i < winCombinations.length; i += 1) {
+      const [a, b, c] = winCombinations[i];
+      if (getSelectors.grids[a].textContent === symbol
+        && getSelectors.grids[b].textContent === symbol
+        && getSelectors.grids[c].textContent === symbol) {
+        getSelectors.winningDisplay.classList.add('show');
+        getSelectors.winningMessage.textContent = symbol;
+        getSelectors.winDiv.textContent = 'WINS';
+        if (symbol === 'X') {
+          getSelectors.winningMessage.classList.add('x-style');
+        } else {
+          getSelectors.winningMessage.classList.add('o-style');
+        }
+        return symbol;
+      }
+    }
+  };
+  return { checkWinningPlayer };
+})();
+// eslint-disable-next-line no-unused-vars
 const playRound = (() => {
-    let gridcount = 0;
-   const player1 = players("X");
-    const player2 = players("O");
-//   updateScreen();
-  grids.forEach((element) => {
+  let gridcount = 0;
+  const player1 = players('X');
+  const player2 = players('O');
+  getSelectors.grids.forEach((element) => {
+    const e = element;
     const handleClick = () => {
       if (player1.turn) {
+        gridcount += 1;
         player1.turn = false;
         player2.turn = true;
-        element.textContent = player1.getSymbol(); 
-        element.classList.add('x-style');
-
-        turnDisplay.classList.remove('x-style');
-        turnDisplay.classList.add('o-style');
-        turnDisplay.textContent = player2.getSymbol();
+        e.textContent = player1.getSymbol();
+        e.classList.add('x-style');
+        getSelectors.turnDisplay.classList.remove('x-style');
+        getSelectors.turnDisplay.classList.add('o-style');
+        getSelectors.turnDisplay.textContent = player2.getSymbol();
+        checkForTie(gridcount);
         checkWin.checkWinningPlayer(player1.getSymbol());
-        gridcount += 1;
       } else if (player2.turn) {
+        gridcount += 1;
         player2.turn = false;
         player1.turn = true;
-        element.textContent = player2.getSymbol();
-        element.classList.add('o-style');
-        turnDisplay.classList.remove('o-style');
-        turnDisplay.classList.add('x-style');
-        turnDisplay.textContent = player1.getSymbol();
+        e.textContent = player2.getSymbol();
+        e.classList.add('o-style');
+        getSelectors.turnDisplay.classList.remove('o-style');
+        getSelectors.turnDisplay.classList.add('x-style');
+        getSelectors.turnDisplay.textContent = player1.getSymbol();
+        checkForTie(gridcount);
         checkWin.checkWinningPlayer(player2.getSymbol());
-        gridcount += 1;
       }
-      if(gridcount>=9){
-        winningDisplay.classList.add('show'); 
-        winningMessage.textContent = 'ITS A TIE!';
-        winDiv.textContent = '';
-      }
-      element.removeEventListener('click', handleClick); 
-     
+
+      element.removeEventListener('click', handleClick);
     };
 
     element.addEventListener('click', handleClick);
-        
   });
-  playagain.addEventListener('click' , ()=>{
-    updateScreen.update();
-  })
+  getSelectors.playagain.addEventListener('click', () => {
+    updateScreen.refresh();
+  });
 })();
-
-const updateScreen = (() =>{
-    const update = () =>{
-         grids.forEach(element => {
-        element.textContent = '';
-    });
-    }
-   return {update}
-})();
-
-
-
-
-const checkWin = (() =>{
-    const winCombinations = [  [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6], 
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8], 
-          [2, 4, 6],
-];
-    const checkWinningPlayer = (symbol) =>{
-        for (let i = 0; i < winCombinations.length; i++) {
-            const [a,b,c] = winCombinations[i];
-            if(grids[a].textContent === symbol && grids[b].textContent === symbol && grids[c].textContent === symbol){
-                console.log(symbol);
-                 winningDisplay.classList.add('show'); 
-                 winningMessage.textContent = symbol;
-                 if(symbol==='X'){
-                    winningMessage.classList.add('x-style');
-                }else{
-                    winningMessage.classList.add('o-style');
-                }
-                return symbol;
-            };
-        }
-        
-       
-    }
-   return {checkWinningPlayer}
-
-})();
-
-
-
